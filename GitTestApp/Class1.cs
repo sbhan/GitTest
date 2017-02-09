988be8dc0894b342cba271bdd99dbd18df6179f0
SungBae.Han <sbhan@captv.co.kr>
2017-02-09 11:06:01 +0900

    Create Solution

---
+++ GitTestApp/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestApp
+{
+    public class Class1
+    {
+        /// 1. Create Solution
+
+    }
+}
